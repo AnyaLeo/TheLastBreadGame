9fe64ee28222e4dba3d68ee113f79fd0612aabdd
Anya <ania.leonova@gmail.com>
2020-09-25 21:04:26 -0700

    added a story explanation before the main scene

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextScene);
+    }
+}
