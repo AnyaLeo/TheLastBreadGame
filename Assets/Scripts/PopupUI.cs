e971a597c67f47bda55989f2f993a9c2abeb073a
Anya <ania.leonova@gmail.com>
2020-09-25 19:29:27 -0700

    Added comments to "PopupUI" script for readability

---
+++ Assets/Scripts/PopupUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/*
+ * Handles spawning a popup with animation
+ * and destroys the animation after the animation is done.
+ * The PopupUI is attached to the parent of the actual animation component
+ * because animator will use the absolute position for the animation.
+ */ 
+public class PopupUI : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        //animator = GetComponent<Animator>();
+        AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+        Destroy(gameObject, clipInfo[0].clip.length);
+    }
+}
