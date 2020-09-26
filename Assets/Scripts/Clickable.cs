7bf76ba4f8713cb4a7c7ddd31bd64e4b1d480721
Anya <ania.leonova@gmail.com>
2020-09-25 19:26:07 -0700

    +1 text appears when clicking on the oven

--- Assets/Scripts/Clickable.cs
+++ Assets/Scripts/Clickable.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 
 /**
  * An object that can be clicked on.
- *
+ * Handles the UI in relation to the clicked object.
  */
 public class Clickable : MonoBehaviour
 {
     public static event Action ObjectClicked = delegate { };
 
+    public GameObject floatingTextPrefab;
+
     public void OnObjectClicked()
     {
         ObjectClicked();
+        spawnUIAtPosition(Input.mousePosition);
+    }
+
+    private void spawnUIAtPosition(Vector2 position)
+    {
+        if (floatingTextPrefab != null)
+        {
+            GameObject instance = Instantiate(floatingTextPrefab);
+            instance.transform.SetParent(transform, false);
+            instance.transform.position = position;
+            Debug.Log(position);
+        }
     }
 }
---
+++ Assets/Scripts/PopupTextUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PopupTextUI : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        //animator = GetComponent<Animator>();
+        AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+        Destroy(gameObject, clipInfo[0].clip.length);
+    }
+}
