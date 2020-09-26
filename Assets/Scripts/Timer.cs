83a09f8e1b9515af2e7cf581e43a737e5462e5e3
Anya <ania.leonova@gmail.com>
2020-09-25 20:29:14 -0700

    added a timer that doesn't do anything except stress you out

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Timer that counts down from maxTime to 0.
+ * Notifies when the timer ran out.
+ */
+public class Timer : MonoBehaviour
+{
+    private float currentTime = 0f;
+    public float maxTime = 30f;
+
+    public Text timerText;
+
+    private void Start()
+    {
+        currentTime = maxTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = "0:" + currentTime.ToString("0");
+        if (currentTime <= 0f)
+        {
+            timerText.text = "0:0";
+        }
+    }
+}
